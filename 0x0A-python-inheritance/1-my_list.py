81b9d0be6bf5b40f2d7dc238e92178886660426d
Maryqueen123 <maryqueenchy@gmail.com>
2023-04-11 15:23:59 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,7 @@ contains the MyList class
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+    """implements sorted printing for the built-in list class"""
 
     def print_sorted(self):
         """prints the sorted list"""
